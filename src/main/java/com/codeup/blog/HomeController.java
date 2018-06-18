f6a97f8919c02ad828fab85f03a5a95a3ca290bb
John <cooks.johns@gmail.com>
2018-06-18 13:55:43 -0500

    more controllers

--- src/main/java/com/codeup/blog/HomeController.java
+++ src/main/java/com/codeup/blog/HomeController.java
@@ -1,4 +1,14 @@
 package com.codeup.blog;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class HomeController {
+    @GetMapping("/")
+    public @ResponseBody
+    String showHomePage() {
+        return "Welcome to my blog!";
+    }
 }
