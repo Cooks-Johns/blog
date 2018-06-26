52e43058793083ecb9bf486aa7e9669d6d2676c7
John <cooks.johns@gmail.com>
2018-06-26 01:33:31 -0500

    setting up the authentication updated files from .testStash like normal I had it working but now its broken again

--- src/main/java/com/codeup/blog/models/User.java
+++ src/main/java/com/codeup/blog/models/User.java
@@ -1,31 +1,29 @@
 package com.codeup.blog.models;
 
-
 import javax.persistence.*;
 import java.util.List;
 
-@Entity@Table(name ="users")
-
+@Entity
+@Table(name = "users")
 public class User {
-    @Id
-    @GeneratedValue
+    @Id @GeneratedValue
     private long id;
 
     @Column(nullable = false, unique = true)
     private String username;
     @Column(nullable = false, unique = true)
     private String email;
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String password;
 
     @OneToMany(mappedBy = "user")
     private List<Post> posts;
 
-    public User() {
-    }
+    public User() {}
 
+    // copy constructor pattern
     public User(User copy) {
-        id = copy.id;
+        id = copy.id; // This line is SUPER important! Many things won't work if it's absent
         email = copy.email;
         username = copy.username;
         password = copy.password;
@@ -35,17 +33,39 @@ public class User {
         return id;
     }
 
-    public void setId(
-            long id,
-            String username,
-            String email,
-            String password,
-            List<Post> posts) {
+    public void setId(long id) {
+        this.id = id;
+    }
 
-                this.id = id,
-                this.username = username,
-                this.email = email,
-                this.password = password,
-                this.posts = posts
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 }
