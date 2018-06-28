e878a914072801d61eb81c4463391e13b0861948
John <cooks.johns@gmail.com>
2018-06-27 21:25:20 -0500

    fixing features

---
+++ src/main/java/.testStash/test.js.kt
@@ -0,0 +1,20 @@
+import pandas as pd
+df1 = pd.read_csv("C:./downloads/Churn_ModChurn_Modelling.csv")
+print(df1)
+
+
+file = "./downloads/Churn_ModChurn_Modelling.csv'
+dfs = pd.read_csv(file, sep="/", header=0,iterator=True, chunksize=1000000, dtype=str)
+
+giant_df = pd.concat(dfs)
+
+print(giant_df.index)
+
+
+file = "./downloads/Churn_ModChurn_Modelling.csv"
+dfs = pd.read_csv(file, sep="/", header=0,iterator=True, chunksize=1000000, dtype=str)
+
+for df in dfs:
+print(df.index)
+
+df.to_csv('output_file.csv', mode='a', index=False)
--- src/main/java/com/codeup/blog/models/Post.java
+++ src/main/java/com/codeup/blog/models/Post.java
@@ -79,3 +79,4 @@ public class Post {
         this.owner = owner;
     }
 }
+
