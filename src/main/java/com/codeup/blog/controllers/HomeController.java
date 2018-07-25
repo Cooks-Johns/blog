cfb95974a51f3a4828ceb7380509be70fa54c9cb
John <cooks.johns@gmail.com>
2018-07-25 03:07:26 -0500

    fixing Home

--- src/main/java/com/codeup/blog/controllers/HomeController.java
+++ src/main/java/com/codeup/blog/controllers/HomeController.java
@@ -1,46 +1,13 @@
 package com.codeup.blog.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.HashMap;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
-    public String showHomePage() {
+    @GetMapping("/home")
+    public String welcome(){
         return "home";
     }
-
-    @GetMapping("/portfolio")
-    public String portfolio() {
-        return "portfolio";
-    }
-
-    @GetMapping("/roll-dice")
-    public String showDiceLinks() {
-        return "roll-dice";
-    }
-
-
-    @GetMapping("/roll/{n}/d{sides}")
-    public @ResponseBody HashMap roll(@PathVariable int n, @PathVariable int sides) {
-        HashMap<String, Object> m = new HashMap<>();
-        Integer[] rolls = new Integer[n];
-        int sum = 0;
-
-        for (int i = 0; i < n; i++) {
-            int random = (int) Math.ceil(Math.random() * sides);
-            rolls[i] = random;
-            sum += random;
-        }
-
-        m.put("rolls", rolls);
-        m.put("sum", sum);
-        return m;
-    }
-
 }
