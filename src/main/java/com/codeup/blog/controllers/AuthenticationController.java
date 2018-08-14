62867155ac324e43db43c8adba8b1923cdc2db99
John <cooks.johns@gmail.com>
2018-08-13 23:52:37 -0500

    fixed the authentication controller

--- src/main/java/com/codeup/blog/controllers/AuthenticationController.java
+++ src/main/java/com/codeup/blog/controllers/AuthenticationController.java
@@ -1,12 +1,22 @@
 package com.codeup.blog.controllers;
 
+import com.codeup.blog.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthenticationController {
+    private final UserRepository userRepository;
+
     @GetMapping("/login")
     public String showLoginForm() {
         return "users/login";
     }
+
+    @Autowired
+    public AuthenticationController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
 }
